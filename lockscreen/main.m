0b1168152cc2d990bae1304b6f64e23e5c1fff6d
Don Reilly <don.reilly@q2ebanking.com>
2014-04-18 11:59:59 -0500

    Initial commit from local git

---
+++ lockscreen/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  lockscreen
+//
+//  Created by Don Reilly on 4/18/14.
+//  Copyright (c) 2014 Don Reilly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
